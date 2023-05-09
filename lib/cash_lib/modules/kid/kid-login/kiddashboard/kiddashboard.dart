fb82263b51feb55ab08d645f53414596fb7ab6ea
selim <selimexiled@gmail.com>
2023-05-10 01:34:51 +0300

    kid

--- lib/cash_lib/modules/kid/kid-login/kiddashboard/kiddashboard.dart
+++ lib/cash_lib/modules/kid/kid-login/kiddashboard/kiddashboard.dart
@@ -1,201 +1,105 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hex_color/flutter_hex_color.dart';
-import 'package:tteesstt/cash_lib/modules/dashboard/add-money/add-money.dart';
-import 'package:tteesstt/cash_lib/modules/dashboard/kids-wallet/kids-wallet.dart';
-import 'package:tteesstt/cash_lib/modules/dashboard/pay/pay.dart';
-import 'package:tteesstt/cash_lib/modules/dashboard/request-money/request-money.dart';
-import 'package:tteesstt/cash_lib/modules/dashboard/send/send.dart';
+import 'package:tteesstt/cash_lib/main_cubit/main_cubit.dart';
+import 'package:tteesstt/cash_lib/modules/authentication/register/register-screen/register-cubit.dart';
+import 'package:tteesstt/cash_lib/modules/dashboard/pay/qr_code/qr_code.dart';
+import 'package:tteesstt/cash_lib/shared/colors/colors.dart';
 
 class KidDashboard extends StatelessWidget {
-  const KidDashboard({Key? key}) : super(key: key);
+  KidDashboard({Key? key}) : super(key: key);
+
+
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: HexColor('303131'),
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(20.0),
-          child: SingleChildScrollView(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                SizedBox(
-                  height: 48,
-                ),
-                Center(
-                  child: Stack(
-                    children: [
-                      Container(
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(17),
-                            gradient: LinearGradient(
-                              begin: Alignment.bottomLeft,
-                              end: Alignment.topRight,
-                              colors: [HexColor('2D776C'), HexColor("12332E")],
-                            )),
-                        width: 349,
-                        height: 134,
-                        child: Column(
-                          children: [
-                            Row(
-                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Padding(
-                                    padding: const EdgeInsets.only(
-                                      left: 21,
-                                      top: 30,
-                                    ),
-                                    child: Text(
-                                        style: TextStyle(
-                                            fontSize: 20,
-                                            fontWeight: FontWeight.bold,
-                                            color: Colors.white),
-                                        "Hi, Mostafa"),
-                                  ),
-                                  IconButton(
-                                    iconSize: 35,
-                                    padding: EdgeInsets.only(
-                                        top: 25,
-                                        right: 27.5),
-                                    color: Colors.white,
-                                    onPressed: () {},
-                                    icon: Icon(Icons.list),
-                                  )
-                                ]),
-                            SizedBox(height: 6,),
-                            Text(
-                                style: TextStyle(
-                                    color: Colors.white,
-                                    fontSize: 16
+      backgroundColor: HexColor('313131'),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal:20,vertical: 20 ),
+        child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(top: 40.0,bottom: 30.0),
+                child: Center(
+                  child: Container(
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(17),
+                        gradient: LinearGradient(
+                          begin: Alignment.bottomLeft,
+                          end: Alignment.topRight,
+                          colors: [HexColor('2D776C'), HexColor("12332E")],
+                        )),
+                    width: 349,
+                    height: 134,
+                    child: Column(
+                      children: [
+                        Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.only(
+                                  left: 21,
+                                  top: 30,
                                 ),
-                                "Your Total Balance"),
-                            SizedBox(height: 6,),
-                            Text(
-                                style: TextStyle(
-                                    color: Colors.white,
-                                    fontSize: 16,
-                                    fontWeight: FontWeight.bold
-                                ),
-                                "EGP 20,000"),
-                          ],
-                        ),
-                      ),
-                    ],
+                                child: Text(
+                                    style: TextStyle(
+                                        fontSize: 20,
+                                        fontWeight: FontWeight.bold,
+                                        color: Colors.white),
+                                    "Hi, Sara Ahmed"),
+                              ),
+                            ]),
+                        SizedBox(height: 6,),
+                        Text(
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 16
+                            ),
+                            "Your Total Balance"),
+                        SizedBox(height: 6,),
+                        Text(
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold
+                            ),
+                            "EGP 150"),
+                      ],
+                    ),
                   ),
                 ),
-                SizedBox(height: 36,),
-                Center(
-                  child: Column(
-                    children: [
-                      Container(
-                        decoration: BoxDecoration(
-                          color: HexColor('6EBEB6'),
-                          borderRadius: BorderRadius.circular(16),
-                        ),
-                        height: 70,
-                        width: 300,
-                        child: IconButton(
-                            color: Colors.white,
-                            onPressed: (){
-                              Navigator.push(context, MaterialPageRoute(builder:  (context) => PayScreen(),));
-                            },
-                            icon: Icon(
-                                size: 33,
-                                Icons.discount)),
-                      ),
-                      SizedBox(height: 4,),
-                      Text(
-                          style:TextStyle(
-                            fontWeight: FontWeight.bold,
-                              fontSize: 30,
-                              color: Colors.white),
-                          "Pay"),
-                    ],
-
-                  ),
-                ),
-                SizedBox(height: 36,),
-                Text(
+              ),
+              Center(
+                child: Text(
                     style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        color: Colors.white,
-                        fontSize: 17
+                      fontSize: 30,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
                     ),
-                    "Lastest Operations"),
-                SizedBox(height: 16,),
-                ListView.separated(
-                    shrinkWrap: true,
-                    physics: NeverScrollableScrollPhysics(),
-                    itemBuilder: (context, index) => Container(
-                      height: 75.0,
-                      width: double.infinity,
-                      decoration: BoxDecoration(
-                          border: Border.all(
-                            color: HexColor('3FACDB'),
-                          ),
-                          borderRadius: BorderRadius.circular(17.0)
-                      ),
-                      child: Padding(
-                        padding: const EdgeInsets.all(10.0),
-                        child: Row(
-                          children: [
-                            CircleAvatar(
-                              backgroundColor:HexColor('3FACDB') ,
-                              child: Icon(
-                                size: 20,
-                                Icons.call_received_outlined,
-                                color: Colors.white,),
-                            ),
-                            SizedBox(width: 8,),
-                            Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                Text('Mohamed Selim',style: TextStyle(
-                                    fontWeight: FontWeight.bold,
-                                    fontSize: 16,
-                                    color: Colors.white
-                                )),
-                                SizedBox(height: 8,),
-                                Row(
-                                  children: [
-                                    Text('10 Feb 2023',style: TextStyle(
-                                      fontSize: 14,
-                                      color: Colors.grey,
-                                    )),
-                                    SizedBox(width: 5,),
-                                    Text('at',style: TextStyle(
-                                      fontSize: 14,
-                                      color: Colors.grey,
-                                    )),
-                                    SizedBox(width: 5,),
-                                    Text('08:00 AM',style: TextStyle(
-                                      fontSize: 14,
-                                      color: Colors.grey,
-                                    )),
-                                  ],
-                                ),
-                              ],
-                            ),
-                            Spacer(),
-                            Text('+ \$700',style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                fontSize: 20,
-                                color: HexColor('3FACDB')
-                            ))
-                          ],
-                        ),
-                      ),
-                    ),
-                    separatorBuilder: (context, index) =>SizedBox(height: 16.0,),
-                    itemCount: 5)
-              ],
-            ),
-          ),
+                    "Scan QR code to pay"),
+              ),
+              SizedBox(height: 8,),
+              SizedBox(
+                height: 100.0,
+                width: 350.0,
+                child: ElevatedButton(
+                  onPressed: ()
+                  {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => QRScan()));},
+                  child: Icon(Icons.qr_code_scanner_outlined,size: 40.0,),
+
+                ),
+              ),
+            ]
         ),
+
       ),
+
+
     );
   }
 }
+
